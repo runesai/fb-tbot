9a1c6e8baa68b1b3dd9d0c321a06f9a4246f093f
runesai <81738591+runesai@users.noreply.github.com>
2021-11-02 17:43:17 +0800

    Update tbot-2.py

--- tbot-2.py
+++ tbot-2.py
@@ -4,8 +4,8 @@ from flask import Flask, request
 from pymessenger import Bot
 
 app = Flask(__name__)
-PAGE_ACCESS_TOKEN = 'EAADX5ZB94LX8BAO3APdUVakmNyOirDXyBgH5GGGwdlc8ZBXg83pD1WcRgKeStefruw5v4P8hqcSUpcM3ZABm1ZAJxtDicwzNKMbli45QAoNlUrE5KlxR067LPNDC2LurX1RgpE4wnGYAuZAwCPebSYgs8U1Qxh7CYKZA9QqYr1yoRARSO782jhgzdgNu2XPzgZD' #os.environ['PAGE_ACCESS_TOKEN']
-VERIFY_TOKEN = 'start_now' #os.environ['VERIFY_TOKEN']
+PAGE_ACCESS_TOKEN = os.environ['PAGE_ACCESS_TOKEN']
+VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot(PAGE_ACCESS_TOKEN)
 
 def process_message(text):
@@ -15,7 +15,7 @@ def process_message(text):
     goodbye = ["Bye", "See you later!"]
 
     if formatted_message == "1" or formatted_message == "one" or formatted_message == "first" or formatted_message == "1st":
-        response = f"{random.choice(greetings}"
+        response = f"{random.choice(greetings)}"
     else:
         response = f"{random.choice(goodbye)}"
     return response
